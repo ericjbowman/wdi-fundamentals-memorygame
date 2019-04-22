cda41558b7d993395416ec5395791a6ac55e1c7b
Eric Bowman <ericbowman@Erics-MacBook-Pro.local>
2019-04-22 01:11:17 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-console.log("user flipped " + cardsInPlay[cardsInPlay.length - 1]);
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardsInPlay[cardsInPlay.length - 1]);
-if (cardsInPlay.length === 2 && cardOne === cardTwo) {
-  alert("You found a match");
-}
-  else {
-    alert("Sorry, try again.");
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
   }
+  else {
+  console.log("Sorry, try again.");
+  }
+}
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+flipCard(0);
+flipCard(2);
